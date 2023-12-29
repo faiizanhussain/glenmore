693c977f43c17d9e02b813db9f25c95c77d9b60c
faiizanhussain <faiizanhussain@gmail.com>
2023-12-29 15:45:52 +0500

    StaticData added

---
+++ Glenmore-Frontend/src/Data/StaticData.tsx
@@ -0,0 +1,298 @@
+export const Content = {
+  MenuItems: ["Home", "About", "Services", "Contact", "Our Journey"],
+
+  Hero_Section: {
+    Title:
+      "IT Advisory that delivers results - Partnering for success and beyond",
+    Link: "#",
+  },
+
+  Features_Section: {
+    Sub_Title: "Why Choose Us?",
+    Title:
+      "Glenmore Consulting is an international consulting firm having a rich history of delivering strategic IT solutions to government, public and private sector customers from across the globe.",
+    Text: "Founded by a team of seasoned professionals with decades of combined experience in this space, our company was built on the belief that technology should empower businesses to achieve excellence to excel in the enterprise world, while fostering business resilience and compliance culture. We are dedicated to providing IT advisory & consulting services and solutions that not only meet your current needs but also prepare you for the challenges of tomorrow.",
+
+    Features: {
+      UnmatchedExpertise: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Unmatched Expertise",
+        Text: "Our team is comprised of the best international IT experts, consultants, and seasoned advisors with a multicultural & diverse background and a deep understanding of multiple industries’ intricacies. We leverage this expertise to deliver tailored solutions that drive tangible results.",
+        link: "#",
+      },
+
+      InnovativetotheCore: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Innovative to the Core",
+        Text: "We are passionate about innovation and technology. Our team is constantly on the lookout for the latest trends and developments in the IT world to ensure that our clients are always ahead of the curve.",
+        link: "#",
+      },
+
+      ClientCentric: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Client Centric",
+        Text: "We prioritize our clients' success above all else. Your goals become our goals, and we work tirelessly to exceed your expectations, offering personalized solutions that align with your unique vision.",
+        link: "#",
+      },
+
+      Execellence: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Execellence",
+        Text: "We strive for excellence in everything we do, from the quality of our services to the relationships we build with our clients.",
+        link: "#",
+      },
+
+      Integrity: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Integrity",
+        Text: "We uphold the highest ethical standards and transparency in all our interactions.",
+        link: "#",
+      },
+      Collaboration: {
+        Image: (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+            />
+          </svg>
+        ),
+        Title: "Collaboration",
+        Text: "We believe that collaboration is key to success and we work closely with our clients, as true partners.",
+        link: "#",
+      },
+    },
+  },
+
+  Services_Section: {
+    Sub_Title: "Our Services",
+    Title:
+      "IT Consulting Services and Solutions for Governments, Public and Private Sector Clients from across various industries around the world.",
+    Text: "At Glenmore Consulting, we offer a comprehensive suite of services designed to empower your organization, enhance your IT capabilities, and drive sustainable success. Our commitment to excellence and innovation ensures that we deliver results that truly matter. Explore our range of services.",
+
+    Features: {
+      "IT Audit & Assurance Services": {
+        Image:
+          "https://maxwellfrank.co.nz/glenmore/wp-content/uploads/2023/11/Rectangle-34.png",
+        Title: "IT Audit & Assurance Services",
+        Text: "Our meticulous approach leaves no stone unturned. As independent auditors, we can facilitate businesses with IT Internal Audit Transformation and review.",
+        link: "#",
+      },
+
+      "Capacity Building and Training Services": {
+        Image:
+          "https://maxwellfrank.co.nz/glenmore/wp-content/uploads/2023/11/Rectangle-34-1.png",
+        Title: "Capacity Building and Training Services",
+        Text: "Invest in your most valuable asset – your people. Our capacity building and training services are designed to empower your team with the knowledge and skills needed to excel in the digital age.",
+        link: "#",
+      },
+
+      "IT Services Management Consultancy": {
+        Image:
+          "https://maxwellfrank.co.nz/glenmore/wp-content/uploads/2023/11/Rectangle-34-2.png",
+        Title: "IT Services Management Consultancy",
+        Text: "Our meticulous approach leaves no stone unturned. As independent auditors, we can facilitate businesses with IT Internal Audit Transformation and review.",
+        link: "#",
+      },
+
+      "Enterprise IT Governance, Risk, and Compliance Advisory": {
+        Image:
+          "https://maxwellfrank.co.nz/glenmore/wp-content/uploads/2023/11/Rectangle-35.png",
+        Title: "Execellence",
+        Text: "In today's complex enterprise IT landscape, effective regulatory compliance is non-negotiable. Our team of IT Governance Specialists help organizations fortify their IT governance structure, enable them to understand the issues and the strategic importance of IT so that they can sustain operations and implement the strategies required to extend their activities into the future.",
+        link: "#",
+      },
+
+      "Benchmarking Advisory on IT Processes": {
+        Image:
+          "https://maxwellfrank.co.nz/glenmore/wp-content/uploads/2023/11/Rectangle-34-5.png",
+        Title: "Benchmarking Advisory on IT Processes",
+        Text: "Our benchmarking and best practices advisory services are tailored to suit your industry and goals. We benchmark your performance against industry best practices, identify opportunities for improvement in operational efficiency and effectiveness, and guide you towards meeting industry standards",
+        link: "#",
+      },
+    },
+  },
+
+  AboutUs_Section: {
+    Title: "A journey towards IT excellence and transformation",
+    Sub_Title: "Let's Begin the Journey",
+    Text: "We invite you to join us on this journey towards IT excellence and transformation. Discover what sets us apart. Contact us today to explore how we can help you achieve your business goals!",
+    Link: "#",
+  },
+
+  Footer_Section: {
+    Consultation_Text: {
+      Title: "Request a consultation & connect with our experts.",
+      IconBoxes: {
+        Phone: {
+          Icon: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+              />
+            </svg>
+          ),
+          Title: "+61 333 2839",
+          link: "tel:+613332839",
+        },
+        Email: {
+          Icon: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+              />
+            </svg>
+          ),
+          Title: "info@glenmore.com",
+          link: "mailto:info@glenmore.com",
+        },
+        Address: {
+          Icon: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Zm0 3h.008v.008h-.008v-.008Z"
+              />
+            </svg>
+          ),
+          Title: "10 Annaluke St. Riverstone NSW 2765 Australia",
+          link: "#",
+        },
+      },
+    },
+    Quick_Links: [
+      {
+        Title: "Enterprise IT Governance, Risk, and Compliance Advisory",
+        Link: "#",
+      },
+      {
+        Title: "IT Services Management Consultancy",
+        Link: "#",
+      },
+      {
+        Title: "Benchmarking Advisory on IT Processes",
+        Link: "#",
+      },
+      {
+        Title: "IT Audit & Assurance Services",
+        Link: "#",
+      },
+      {
+        Title: "Capacity Building and Training Services",
+        Link: "#",
+      },
+    ],
+
+    Footnote: {
+      Text1: {
+        Title: "© 2021 Glenmore Consulting. All Rights Reserved.",
+        Link: "#",
+      },
+
+      Text2: {
+        Title: "Designed@QuivLabs",
+        Link: "https://quivlabs.com/",
+      },
+    },
+  },
+};
